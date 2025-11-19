7b0cba36cf0a29ecc6bea83b356e4932dc3320f4
Farh Kadry <23-101289@students.eui.edu.eg>
2025-11-19 19:54:10 +0200

    2 sections added, interactions and animations adjusted

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+// let parent = document.getElementById("leafParent");
+// let model  = document.getElementById("leafModel");
 
+// // ORBITS
+// let defaultOrbit =  "0deg 0deg 1m";
+// let scrollOrbit  =  "0deg 0deg 0.5m";
+// let hoverOrbit   = "0deg 0deg 0.5m";
+
+// // SCROLL
+// window.addEventListener("scroll", () => {
+//     if (window.scrollY > 100) {
+//         model.cameraOrbit = scrollOrbit;
+//     } else {
+//         model.cameraOrbit = defaultOrbit;
+//     }
+// });
+
+// // MOUSEENTER PARENT
+// parent.addEventListener("mouseenter", () => {
+//     model.cameraOrbit = hoverOrbit;
+// });
+
+// // MOUSELEAVE PARENT
+// parent.addEventListener("mouseleave", () => {
+//     model.cameraOrbit = defaultOrbit;
+// });
