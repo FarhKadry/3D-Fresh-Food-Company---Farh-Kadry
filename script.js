3adb263efaf350f263aaccdc0a1af3a817a6ce2f
FarhKadry <23-101289@students.eui.edu.eg>
2025-11-20 18:16:56 +0200

    homepage array added

--- script.js
+++ script.js
@@ -24,4 +24,44 @@
 // parent.addEventListener("mouseleave", () => {
 //     model.cameraOrbit = defaultOrbit;
 // });
+const cards = [
+  {
+    icon: "public/images/recycle.svg",
+    title: "Recyclable Packaging",
+    desc: "Track every step from farm to your doorstep",
+    posClass: ""
+  },
+  {
+    icon: "public/images/electricity.svg",
+    title: "Transparent Supply Chain",
+    desc: "Track every step from farm to your doorstep",
+    posClass: "offers_card_position2"
+  },
+  {
+    icon: "public/images/recycle.svg",
+    title: "Recyclable Packaging",
+    desc: "Track every step from farm to your doorstep",
+    posClass: ""
+  },
+  {
+    icon: "public/images/sun.svg",
+    title: "Natural Sunlight",
+    desc: "Track every step from farm to your doorstep",
+    posClass: "offers_card_position2"
+  }
+]; const container = document.getElementById("cardsContainer");
 
+cards.forEach(card => {
+  container.innerHTML += `
+    <div class="offers_card move_focus scroll_animate ${card.posClass}">
+      <div class="title">
+        <img src="${card.icon}">
+        <h4 class="text_bold h4_2">${card.title}</h4>
+        <p class="text_regular">${card.desc}</p>
+        <button class="learn_more">
+          Learn more <img src="public/images/arrowgreen.svg" alt="">
+        </button>
+      </div>
+    </div>
+  `;
+});
