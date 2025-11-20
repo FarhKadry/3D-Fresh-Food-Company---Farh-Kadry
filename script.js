fa1e75e5f30026ceeee0eabac96903af6f87650b
Farh Kadry <23-101289@students.eui.edu.eg>
2025-11-20 21:32:57 +0200

    media corrected, javascript usages added

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
-// let parent = document.getElementById("leafParent");
-// let model  = document.getElementById("leafModel");
+// CARDS ARRAY
+let cards = [
+  {
+    icon: "public/images/recycle.svg",
+    title: "Recyclable Packaging",
+    desc: "Track every step from farm to your doorstep",
+    positionClass: ""
+  },
+  {
+    icon: "public/images/electricity.svg",
+    title: "Transparent Supply Chain",
+    desc: "Track every step from farm to your doorstep",
+    positionClass: "offers_card_position2"
+  },
+  {
+    icon: "public/images/recycle.svg",
+    title: "Recyclable Packaging",
+    desc: "Track every step from farm to your doorstep",
+    positionClass: ""
+  },
+  {
+    icon: "public/images/sun.svg",
+    title: "Natural Sunlight",
+    desc: "Track every step from farm to your doorstep",
+    positionClass: "offers_card_position2"
+  }
+];
+let container = document.getElementById("cardsContainer1");
+cards.forEach(card => {
+  container.innerHTML += `
+    <div class="offers_card move_focus scroll_animate ${card.positionClass}">
+      <div class="title">
+        <img src="${card.icon}">
+        <h4 class="text_bold h4_2">${card.title}</h4>
+        <p class="text_regular">${card.desc}</p>
+        <button class="learn_more">
+          Learn more <img src="public/images/arrowgreen.svg" alt="">
+        </button>
+      </div>
+    </div>
+  `;
+});
 
-// // ORBITS
-// let defaultOrbit =  "0deg 0deg 1m";
-// let scrollOrbit  =  "0deg 0deg 0.5m";
-// let hoverOrbit   = "0deg 0deg 0.5m";
+// ARPAGE1
+const arButton1 = document.getElementById("arButton1");
+const productModel1 = document.getElementById("productModel1");
 
-// // SCROLL
-// window.addEventListener("scroll", () => {
-//     if (window.scrollY > 100) {
-//         model.cameraOrbit = scrollOrbit;
-//     } else {
-//         model.cameraOrbit = defaultOrbit;
-//     }
-// });
-
-// // MOUSEENTER PARENT
-// parent.addEventListener("mouseenter", () => {
-//     model.cameraOrbit = hoverOrbit;
-// });
-
-// // MOUSELEAVE PARENT
-// parent.addEventListener("mouseleave", () => {
-//     model.cameraOrbit = defaultOrbit;
-// });
-
+arButton1.addEventListener("click", () => {
+  if (productModel1.activateAR) {
+    productModel1.activateAR();
+  } else {
+    alert("AR is not supported on this device.");
+  }
+});
